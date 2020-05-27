e76996d280c56f37c7ff35f5d8d61414ac431e84
Vipul Sinha <vsasvipul@gmail.com>
2020-05-27 09:44:07 +0530

    add product by id

--- controllers/products.ts
+++ controllers/products.ts
@@ -52,3 +52,26 @@ const getProduct = ({ params, response }: { params: { id : string}, response : a
         }
     }
 }
+
+// @desc    Add product
+// @route    Post api/v1/products
+const addProduct = async ({ request, response }: {request: any, response : any}) => {
+    const body = await request.body()
+
+    if(!request.hasBody) {
+        response.status = 400
+        response.body = {
+            success : false,
+            msg : 'No data'
+        }
+    } else {
+        const product: Product = body.value
+        product.id = v4.generate()
+        products.push(product)
+        response.status = 201
+        response.body = {
+            success : true,
+            data : product
+        }
+    }
+}
