f2c304f870fee520d50904d2ad1d36a2b550f361
Vipul Sinha <vsasvipul@gmail.com>
2020-05-27 09:44:44 +0530

    update product by id

--- controllers/products.ts
+++ controllers/products.ts
@@ -75,3 +75,29 @@ const addProduct = async ({ request, response }: {request: any, response : any})
         }
     }
 }
+
+// @desc    Update product
+// @route    Put api/v1/products/:id
+const updateProduct = async({ params, request, response }: { params: {id:string},request:any, response : any}) => {
+    const product: Product | undefined = products.find(p => p.id === params.id)
+
+    if(product) {
+        const body = await request.body()
+
+        const updateData : { name ?: string; description?: string; price?: number} = body.value
+        
+        products = products.map(p=> p.id === params.id ? { ...p, ...updateData } : p)
+    
+        response.status = 200
+        response.body = {
+            success : true,
+            data : products
+        }    
+    } else {
+        response.status = 404
+        response.body = {
+            success : false,
+            msg : 'No product found'
+        }
+    }
+}
